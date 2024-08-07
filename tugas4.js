7a481ae109f1def5cd0ecc474ed23db8583d0971
MuhammadBeryl <berlygalek23@gmail.com>
2024-08-07 14:20:11 +0700

    Modul Praktikum

--- tugas4.js
+++ tugas4.js
@@ -1,10 +1,14 @@
-let a = 4
-let r  = 3
-let n = 10
-let jumlah = 0
+let a = 4;
+let r = 3;
+let n = 10;
+let jumlah = 0;
+
+console.log("Suku-suku deret geometri:");
 
 for (let i = 0; i < n; i++) {
-    jumlah += a * Math.pow(r, i)
+    let suku = a * Math.pow(r, i);
+    console.log("Suku ke-" + (i + 1) + ": " + suku);
+    jumlah += suku;
 }
 
-console.log("Jumlah 10 suku pertama dari deret geometri adalah: " + jumlah)
+console.log("Jumlah 10 suku pertama dari deret geometri adalah: " + jumlah);
